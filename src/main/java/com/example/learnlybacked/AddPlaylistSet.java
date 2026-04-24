703f4101f08ce0f028a37f2a3b8d07ce82021027
Forek145 <pilarczyk.miko@gmail.com>
2026-04-24 02:49:42 +0200

    utworzenie tabel do odbioru danych z reacta

--- src/main/java/com/example/learnlybacked/AddPlaylistSet.java
+++ src/main/java/com/example/learnlybacked/AddPlaylistSet.java
@@ -1,18 +1,42 @@
 package com.example.learnlybacked;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:5173")
 public class AddPlaylistSet
 {
+    @Data
+    class Song
+    {
+        String title;
+        String url;
+    }
+
+    @Data
+    class Playlist
+    {
+        String title;
+        List<Song> songs;
+    }
+
+    @Data
+    class PlaylistSetFormData
+    {
+        String title;
+        String category;
+        String[] tags;
+        List<Playlist> playlists;
+    }
+
+
     @PostMapping("/upload")
-    public String RecivePlaylistSet()
+    public String RecivePlaylistSet(@RequestBody PlaylistSetFormData data)
     {
 
         return "";
